505ccc37d2a8516b3a5761f04a1850c9777a66d0
Agarwal, Ankit-CW <Ankit.Agarwal@bestbuy.com>
2019-07-21 22:11:07 -0700

    state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import Person from './Person/Person.js';
 //function App() {
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 31},
+      {name: 'Stephanie', age: 32}
+    ]
+  }
  render(){
   return (
     // this is not an html tag, its a tag provided by react
 //className is used since its not html, and class is a reserved keyword
     <div className="App">
-      <h1> Hi Bubu, I just wanted to let you know that......</h1>
+      <h1> Hi, I`m a React App!</h1>
+      <p>This is working!</p>
+      <button> Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Stock market trading</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My Hobbies: Stock market trading</Person>
+      <Person />
       <Person />
     </div>
   );
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = () => {
-  return <p>{Math.random()}You are awesome and thank you for being so sweet! I love you!!!!</p>
+const person = (props) => {
+  return (
+    <div>
+<p>{Math.random() * 30} I am a Person and my name is {props.name} and I am {props.age} years old</p>
+<p>{props.children}</p>
+    </div>
+  )
+
 }
 
 export default person;
