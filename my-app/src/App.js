a405c3188bd7b78d00761eaa4bfca6d9b772172b
Agarwal, Ankit-CW <Ankit.Agarwal@bestbuy.com>
2019-07-21 21:26:20 -0700

    updating components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person.js';
 //function App() {
 class App extends Component {
-render(){
+ render(){
   return (
+    // this is not an html tag, its a tag provided by react
+//className is used since its not html, and class is a reserved keyword
     <div className="App">
-      <h1> Hi, I`m a React App</h1>
+      <h1> Hi Bubu, I just wanted to let you know that......</h1>
+      <Person />
     </div>
   );
+  //the above lines of code would translate into javascript which would look like whats happening below.
+// return (React.createElement('div', null, 'Hi, I `m a React App!!'))
+ // return (React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I `m a React App!!')))
 }
 }
 
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+  return <p>{Math.random()}You are awesome and thank you for being so sweet! I love you!!!!</p>
+}
+
+export default person;
