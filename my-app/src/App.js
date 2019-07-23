3aa76c97daeb13ca20199f24fe55a2e204cce362
Agarwal, Ankit-CW <Ankit.Agarwal@bestbuy.com>
2019-07-23 13:58:16 -0700

    using props to set state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,10 +11,10 @@ state = {
   {name: 'Stephanie', age: 32}
 ]
 }
-switchNameHandler = () => {
+switchNameHandler = (newName) => {
   this.setState({
     persons: [
-    {name: 'Maximilian', age: 280},
+    {name: newName, age: 280},
     {name: 'Manu', age: 310},
     {name: 'Steph', age: 320}
   ]
@@ -28,11 +28,11 @@ render (){
       <div className="App">
         <h1> Hi, I`m a React App!</h1>
         <p>This is working!</p>
-        <button onClick={this.switchNameHandler}> Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Lilly!!!!')}> Switch Name</button>
         <Person
         name={this.state.persons[0].name}
          age={this.state.persons[0].age}
-         click={this.switchNameHandler}/>
+         click={this.switchNameHandler.bind(this, 'Lilly')}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Stock market trading</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My Hobbies: Stock market trading</Person>
         <Person />
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-<p onclick={props.click}>{Math.random() * 30} I am a Person and my name is {props.name} and I am {props.age} years old</p>
+<p onClick={props.click}>{Math.random() * 30} I am a Person and my name is {props.name} and I am {props.age} years old</p>
 <p>{props.children}</p>
     </div>
   )
